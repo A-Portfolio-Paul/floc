8860aacf383fe2bba8c2986cf832e8924cd07b3b
PaulAyling <jimmyyukka@gmail.com>
2022-02-27 11:12:18 +0100

    can now update todos

---
+++ src/lib/Todo.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let todo, updateTodo;
+</script>
+
+<div class="todo">
+	<input type="checkbox" checked={todo.isComplete} />
+	<input
+		type="text"
+		value={todo.task}
+		on:input={(e) => {
+			console.log('input clicked');
+			todo.task = e.currentTarget.value;
+			updateTodo(todo);
+		}}
+	/>
+	<button>Delete</button>
+</div>
+
+<style>
+	.todo {
+		display: flex;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,30 @@
 <script>
 	import { onMount } from 'svelte';
 	import supabase from '$lib/db';
+	import Todo from '../lib/Todo.svelte';
 
 	let todos = [];
 	onMount(async () => {
-        let { data, error } = await supabase.from("todos").select("*");
+		let { data, error } = await supabase.from('todos').select('*');
 		todos = data;
 	});
+
+	const updateTodo = async (todo) => {
+		console.log('updateTodo running....');
+		console.table(todo);
+		try {
+			const { data, error } = await supabase
+				.from('todos')
+				.update({ task: todo.task })
+				.eq('id', todo.id);
+		} catch {
+			console.log(err);
+		}
+	};
 </script>
 
 {#each todos as todo}
-<div class="todo">
-    <input type="checkbox" checked={todo.isComplete}>
-    <input type="text" value={todo.task}>
-    <button>Delete</button>
-</div>
+	<Todo {todo} {updateTodo} />
 {:else}
-<p>no todos found</p>
-
-{/each }
-<style>
-    .todo{
-        display:flex;
-    }
-</style>
+	<p>no todos found</p>
+{/each}
